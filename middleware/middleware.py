7af4992db6d800a0cfb4e831a350a3a3d5fded50
rnetonet <rneto@rneto.net>
2018-11-01 06:49:06 -0300

    config files support

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,13 +1,18 @@
+import configparser
+import os
 import time
 
 import paho.mqtt.client as paho
 
-_BROKER = "localhost"
+this_module_path = os.path.split(__file__)[0]
+config_file = os.path.join(this_module_path, "middleware.ini")
+
+config = configparser.ConfigParser()
+config.read(config_file)
 
 
 def on_message(client, userdata, message):
     print("------------------------------")
-    print(f"client: {client.}")
     print("topic: %s" % message.topic)
     print(message.payload.decode("utf8"))
     print("qos: %d" % message.qos)
@@ -17,6 +22,6 @@ def on_message(client, userdata, message):
 client = paho.Client()
 client.on_message = on_message
 
-client.connect(_BROKER)
+client.connect(config["middleware"]["broker_host"])
 client.subscribe("enviot/#")
 client.loop_forever()
--- perception/perception_simulator.py
+++ perception/perception_simulator.py
@@ -1,30 +1,37 @@
+import configparser
 import datetime
+import os
 import random
 import time
 
 import paho.mqtt.client as paho
 import pandas as pd
 
-_DELAY = 30
-_DATAFILE = "data.csv"
-_BROKER = "localhost"
+this_module_path = os.path.split(__file__)[0]
+config_file = os.path.join(this_module_path, "perception.ini")
 
-df = pd.read_csv(_DATAFILE)
-df["reading_time"] =  pd.to_datetime(df["reading_time"], yearfirst=True)
+config = configparser.ConfigParser()
+config.read(config_file)
+
+data_file = os.path.join(this_module_path, config["perception"]["data_file"])
+
+df = pd.read_csv(data_file)
+df["reading_time"] = pd.to_datetime(df["reading_time"], yearfirst=True)
 df.index = df["reading_time"]
 
 client = paho.Client()
-client.connect(_BROKER)
+client.connect(config["perception"]["broker_host"])
 
 while True:
     current_hour = datetime.datetime.now().hour
     hour_start = f"{current_hour:02}:00"
     hour_end = f"{current_hour:02}:59"
     data = df.between_time(hour_start, hour_end)
-    
-    payload = data.sample().to_json()
 
+    payload = data.sample().to_json(orient="records")
+    
     client.publish(f"enviot/notify", payload)
-    time.sleep(_DELAY)
+
+    time.sleep(config.getint("perception", "interval", fallback=30))
 
 client.disconnect()
